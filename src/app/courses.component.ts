3c20d65fd19898624b85fb71348920f3dbfca946
Arun Nair <arun.nair@cygrp.com>
2019-10-23 16:22:14 +0530

    for deployment

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,35 +3,7 @@ import { coursesService } from './courses.service';
 @Component(
     {
         selector:'courses',
-        template:`<h2>Course Component Content</h2><p>{{ "first attempt : "+second_title }}</p>
-        <p>{{ "using function => " + getTitle() }}</p>
-        <button class="btn btn-primary" (click)="onClickDiv()">Submit button</button>
-        <br/>
-        <div (click)="onClickDiv()">
-        <button [class.btn-primary]="isActive" (click)="onClickButton($event)">SAve button</button>
-        </div>
-        <br/>
-        <button [style.color]="isActive2 ? 'red' : 'green'">Ok button</button>
-        
-        <ul>
-        below displaying list using interpolation
-            <li *ngFor="let course of courses">
-            {{ course }}
-            </li>
-        </ul>
-        
-        
-        <input type="text" (keyup)="keyupMethod1($event)"/>
-
-        <input type="text" (keyup)="keyupMethod()"/>
-        
-        <input type="text" #email (keyup.enter)="keyupMethod(email.value)" placeholder="#email"/>
-
-        <input type="text" placeholder="Two way binding" [(ngModel)]="two_Way_mail" (keyup.enter)="twowaymethod()"/>
-
-
-       
-        `
+        template:`<h2>Course Component Content</h2>`
 
 
     }
